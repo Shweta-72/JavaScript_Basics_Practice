b45660848989c26ce4e46e305bea91063327b74c
Shweta-44 <150121563+Shweta-72@users.noreply.github.com>
2023-11-29 20:30:36 +0530

    data types in javascript

---
+++ JavaScript_Practice/script.js
@@ -0,0 +1,29 @@
+console.log("Hello");
+// Data Types ---------------------------
+
+// variables, constants, comments
+
+// VAR--------
+var score = 34;
+var score = 56;
+console.log(score);
+
+// LET--------
+let points = 23;
+// let points = 43; // throws error
+points = 43;
+console.log(points);
+
+//  CONST-----
+const value = 33;
+// const value = 43; // throws error
+// value = 44;  // throws error
+console.log(value);
+
+// let score = null;
+// score = 33;
+// console.log(score);
+
+// undefined-----------
+let undefinedCheck;
+console.log(undefinedCheck);
